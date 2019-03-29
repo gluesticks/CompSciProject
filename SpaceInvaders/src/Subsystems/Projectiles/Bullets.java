d832782197437c17e473e5d81901ca06a72b058c
Jake Bryant <jmkh259@gmail.com>
2019-03-29 12:04:14 -0500

    created classes and added comments

---
+++ SpaceInvaders/src/Subsystems/Projectiles/Bullets.java
@@ -0,0 +1,23 @@
+package Subsystems.Projectiles;
+
+public class Bullets
+{
+    public void hitDetection()
+    {
+
+    }
+    public void damage(int damage, int healthStat)
+    {
+
+    }
+    public void boundaryCheck(int bulletX, int bulletY)
+    {
+
+    }
+    public void bulletFire()
+    {
+        //at some random point in time the enemy will fire a bullet
+        //if (set amount of time) passes
+            //wait set random interval of time to fire bullet
+    }
+}
