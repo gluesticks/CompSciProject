2f7fd4283e87dfd72cd806cd49b5cda58a2efa8f
Jake Bryant <jmkh259@gmail.com>
2019-03-29 15:40:04 -0500

    Added classes and comments

---
+++ SpaceInvaders/src/Subsystems/Sprites/SEnemy.java
@@ -0,0 +1,26 @@
+package Subsystems.Sprites;
+
+public class SEnemy
+{
+    protected void sprite()
+    {
+        //import the sprite from the folder
+    }
+    public void hitbox()
+    {
+        //determine how large the hitbox is
+    }
+    public boolean fire(boolean fire)
+    {
+        //hold a random time interval
+        //if the time passed equals that interval 
+            //fire bullet
+        //else wait for a certain period
+        return fire;
+    }
+    public void boundaryCheck(int enemyX, int enemyY)
+    {
+        //if eneny pos is outside the screen
+            //bounce back into boundary
+    }
+}
