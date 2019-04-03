ebd33a3b92056e987f27eff98efa1f49813d0426
Jake Bryant <jmkh259@gmail.com>
2019-04-03 11:41:13 -0500

    created classes and added pseudo code

---
+++ SpaceInvaders/src/Subsystems/Projectiles/Asteroids.java
@@ -0,0 +1,14 @@
+package Subsystems.Projectiles;
+
+public class Asteroids
+{
+    public void move(int x, int y)
+    {
+        //x += Math.rand()
+        //y += Maht.rand()
+    }
+    public boolean hitDetection(boolean hit)
+    {
+        return hit;
+    }
+}
