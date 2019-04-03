a952540baf101346e72ce7a689366534cd1e3ab6
Jake Bryant <jmkh259@gmail.com>
2019-04-03 11:16:40 -0500

    Created new classes and added comments

--- SpaceInvaders/src/Subsystems/Projectiles/Bullets.java
+++ SpaceInvaders/src/Subsystems/Projectiles/Bullets.java
@@ -2,6 +2,23 @@ package Subsystems.Projectiles;
 
 public class Bullets
 {
+    public int getBulletPos()
+    {
+        //call class to get sprite pos
+        return bulletPos;
+    }
+    public void move(int x, int y, int screenX, int screenY)
+    {
+        while(x < screenX && y < screenY)
+        {
+            x += 3;
+            y += 3;
+        }
+        if( x > screenX || x < screenX || y > screenY || y < screenY)
+        {
+            //despawn bullet
+        }
+    }
     public void hitDetection()
     {
 
@@ -10,9 +27,13 @@ public class Bullets
     {
 
     }
-    public void boundaryCheck(int bulletX, int bulletY)
+    public boolean boundaryCheck(int bulletX, int bulletY, int screenX, int screenY, boolean inBounds)
     {
-
+        //if bulletX <= screenX and bulletY <= screenY
+            //set inBounds = true
+        //elif bulletX > screenX or bulletY > screenY
+            //set inBounds = false
+            return inBounds;
     }
     public void bulletFire()
     {
