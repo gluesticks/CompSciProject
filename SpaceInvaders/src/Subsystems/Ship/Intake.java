0564b24cedf74788ac13e42027ba6dcf610efe93
Rocker625 <jlarock711@gmail.com>
2019-04-22 11:48:00 -0500

    Update Intake.java

--- SpaceInvaders/src/Subsystems/Ship/Intake.java
+++ SpaceInvaders/src/Subsystems/Ship/Intake.java
@@ -2,6 +2,13 @@ package Subsystems.Ship;
 
 //The Intake upgrade for the Ship class
 
-public class Intake extends Ship {
-
+public class Intake
+{
+	boolean bought = false;
+	int level = 0;
+	
+	public Intake()
+	{
+		
+	}
 }
